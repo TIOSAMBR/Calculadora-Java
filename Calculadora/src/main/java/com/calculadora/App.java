9afa7ef0b16cc47e994747bb270b40d64e01ac1f
Samuel Toledo <102129312+TIOSAMBR@users.noreply.github.com>
2023-11-23 15:30:20 -0300

    Calculadora 2.0
    Calculadora 2.0

--- Calculadora/src/main/java/com/calculadora/App.java
+++ Calculadora/src/main/java/com/calculadora/App.java
@@ -18,7 +18,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 240, 200);
+        scene = new Scene(loadFXML("primary"), 200, 235);
         stage.setScene(scene);
         stage.show();
     }
--- Calculadora/src/main/java/com/calculadora/PrimaryController.java
+++ Calculadora/src/main/java/com/calculadora/PrimaryController.java
@@ -10,11 +10,20 @@ public class PrimaryController {
     @FXML
     private TextField tela;
 
-    private StringBuilder expressao = new StringBuilder();
+    private StringBuilder expressao = new StringBuilder("0");
+    
+     @FXML
+    public void initialize() {
+        expressao = new StringBuilder("0");
+        atualizarTela();
+    }
 
     @FXML
     private void adicionarNumero(ActionEvent event) {
         Button button = (Button) event.getSource();
+        if (expressao.toString().equals("0")) {
+            expressao.setLength(0);
+        }
         expressao.append(button.getText());
         atualizarTela();
     }
@@ -22,48 +31,50 @@ public class PrimaryController {
     @FXML
     private void adicionarOperacao(ActionEvent event) {
         Button button = (Button) event.getSource();
-        expressao.append(" ").append(button.getText()).append(" ");
-        atualizarTela();
+        if (!expressao.toString().equals("0")) {
+            expressao.append(" ").append(button.getText()).append(" ");
+            atualizarTela();
+        }
     }
 
     @FXML
     private void limpar() {
         expressao.setLength(0);
+        expressao.append("0");
         tela.clear();
+        atualizarTela();
     }
 
     private void atualizarTela() {
         tela.setText(expressao.toString());
     }
+
     @FXML
-    
 private void calcular() {
-    if (expressao.length() > 0) {
+    if (!expressao.toString().equals("0")) {
         String[] partes = expressao.toString().split(" ");
-        
+
         if (partes.length == 3) {
             try {
                 double num1 = Double.parseDouble(partes[0]);
                 String operacao = partes[1];
                 double num2 = Double.parseDouble(partes[2]);
-                
+
+                double resultado = 0;
+
                 switch (operacao) {
                     case "+":
-                        expressao.setLength(0);
-                        expressao.append(num1 + num2);
+                        resultado = num1 + num2;
                         break;
                     case "-":
-                        expressao.setLength(0);
-                        expressao.append(num1 - num2);
+                        resultado = num1 - num2;
                         break;
                     case "*":
-                        expressao.setLength(0);
-                        expressao.append(num1 * num2);
+                        resultado = num1 * num2;
                         break;
                     case "/":
                         if (num2 != 0) {
-                            expressao.setLength(0);
-                            expressao.append(num1 / num2);
+                            resultado = num1 / num2;
                         } else {
                             return;
                         }
@@ -71,13 +82,24 @@ private void calcular() {
                     default:
                         break;
                 }
-                
+
+                // Verificar se o resultado tem parte decimal
+                if (resultado % 1 == 0) {
+                    // Resultado é um número inteiro
+                    expressao.setLength(0);
+                    expressao.append((int) resultado);
+                } else {
+                    // Resultado tem parte decimal
+                    expressao.setLength(0);
+                    expressao.append(resultado);
+                }
+
                 tela.setText(expressao.toString());
-                
+
             } catch (NumberFormatException e) {
             }
         }
     }
+    }
 }
 
-}
